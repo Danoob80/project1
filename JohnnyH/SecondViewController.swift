8be94d7e99e83269e89683874dbabc721ab0b9e3
Daniel CAMBIEN <daniel.cambien@sdw-100-16.local>
2017-12-06 14:06:14 +0100

    first commit

---
+++ JohnnyH/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  JohnnyH
+//
+//  Created by Daniel CAMBIEN on 06/12/2017.
+//  Copyright © 2017 Daniel CAMBIEN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
